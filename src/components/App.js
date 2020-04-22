8f400cace40c5e64bc72841b9d009fbe0029df8f
memematvaz <you@example.com>
2020-04-22 19:19:08 +0200

    components and character

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 import React from 'react';
+import '../styles/App.scss';
+import fetchCharacters from './services/FetchData.js';
+//import CharacterList from './ShowList';
+//import CharacterDetails from './CharacterDetails';
+//import FilterInput from './FilterInput';
+//import { Route, Switch } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        }
+  }
+
+
+  componentDidMount(){
+    fetchCharacters()
+      .then(data => {
+        this.setState ({
+          data:data
+        })
+      })
+    }
+  }
+
+  export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const Character = (props) => {
+  return (
+    <div>
+      <img src={props.image} alt={props.name}/>
+      <h2>{props.name}</h2>
+      <p>{props.specie}</p>
+    </div>
+  )
+}
+
+export default Character;
