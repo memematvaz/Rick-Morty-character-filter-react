20acf1d0d0b2fed32847029171f3dc2140280d01
memematvaz <you@example.com>
2020-04-23 21:18:15 +0200

    text-error

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Character from './Character.js';
 import { Link } from 'react-router-dom';
-import NoFoundMessage from './NoFoundMessage.js';
 
 const CharacterList = (props) => {
+
+    const results = [ ...props.data ]
+        .filter(characterObject => props.inputValue === '' || characterObject.name.toLowerCase().includes(props.inputValue.toLowerCase()));
     
     console.log('busqueda' + props.data.length)
-    if(props.data.length===0) {
+    if(results.length===0) {
         
-        return <NoFoundMessage/>}
+        return <p>{`There is no character named ${props.inputValue}`}</p>}
 
     else{
         return(
                 <ul>
-                {props.data
-                    .filter(characterObject => props.inputValue === '' || characterObject.name.toLowerCase().includes(props.inputValue.toLowerCase()))
+                {results
                     .map(characterObject =>
         
                     <li key={characterObject.id}>
@@ -33,56 +34,3 @@ const CharacterList = (props) => {
     
 }
 export default CharacterList;
-
-/*if(props.data.length !== props.inputValue){
-        return (`There is no character named ${props.inputValue}`)
-    }else{
-     return(
-        <ul>
-        {props.data
-            .filter(characterObject => props.inputValue === '' || characterObject.name.toLowerCase().includes(props.inputValue.toLowerCase()))
-            .map(characterObject =>
-
-            <li key={characterObject.id}>
-                <Link to={`/character/${characterObject.id}`}>
-                    <Character image={characterObject.image}
-                               name={characterObject.name}
-                               specie={characterObject.species}
-                    />
-                </Link>
-            </li>
-            )}
-        </ul>
-    )
-}
-*/
-
-/*const CharacterList = (props) => {
-    console.log(props.inputValue)
-    if(characterObject => props.data.includes(characterObject.name)) {
-        
-        return ( 
-                <ul>
-                {props.data
-                    .filter(characterObject => props.inputValue === '' || characterObject.name.toLowerCase().includes(props.inputValue.toLowerCase()))
-                    .map(characterObject =>
-        
-                    <li key={characterObject.id}>
-                        <Link to={`/character/${characterObject.id}`}>
-                            <Character image={characterObject.image}
-                                       name={characterObject.name}
-                                       specie={characterObject.species}
-                            />
-                        </Link>
-                    </li>
-                    )}
-                </ul>
-            )
-        }
-    else{
-     return( `There is no character named ${props.inputValue}`)
-       
-    
-}
-
-}*/
