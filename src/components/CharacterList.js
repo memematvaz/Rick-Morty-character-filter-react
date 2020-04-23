d5906c89297daeb9c0b85feae5e96f8bf9ce055f
memematvaz <you@example.com>
2020-04-23 21:22:03 +0200

    no-found-text

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,28 +3,34 @@ import Character from './Character.js';
 import { Link } from 'react-router-dom';
 
 const CharacterList = (props) => {
-    console.log(props.inputValue)
-    return (
-        <ul>
-        {props.data
-            .filter(characterObject => props.inputValue === '' || characterObject.name.toLowerCase().includes(props.inputValue.toLowerCase()))
-            .map(characterObject =>
 
-            <li key={characterObject.id}>
-                <Link to={`/character/${characterObject.id}`}>
-                    <Character image={characterObject.image}
-                               name={characterObject.name}
-                               specie={characterObject.species}
-                    />
-                </Link>
-            </li>
-            )}
-        </ul>
-    )
+    const results = [ ...props.data ]
+        .filter(characterObject => props.inputValue === '' || characterObject.name.toLowerCase().includes(props.inputValue.toLowerCase()));
+    
+    console.log('busqueda' + props.data.length)
+    if(results.length===0) {
+        
+        return <p>{`There is no character named ${props.inputValue}`}</p>}
+
+    else{
+        return(
+                <ul>
+                {results
+                    .map(characterObject =>
+        
+                    <li key={characterObject.id}>
+                        <Link to={`/character/${characterObject.id}`}>
+                            <Character image={characterObject.image}
+                                       name={characterObject.name}
+                                       specie={characterObject.species}
+                            />
+                        </Link>
+                    </li>
+                    )}
+                </ul>
+            
+        )
+    }
+    
 }
-
-
-
-
-
 export default CharacterList;
