b74642b655029e9280d3453164ecc23b9c32ab64
memematvaz <memematvaz@gmail.com>
2020-04-24 20:35:11 +0200

    docs

---
+++ docs/precache-manifest.72d87774a183a7c3f048f2af5a5db1cb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb890759342ec60be28897aa21d5e8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0ae90db0a19d9f285f3",
+    "url": "./static/css/main.447c31b0.chunk.css"
+  },
+  {
+    "revision": "ce041bd1019b1ab72bff",
+    "url": "./static/js/2.c27e38e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c27e38e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ae90db0a19d9f285f3",
+    "url": "./static/js/main.c39d2ee9.chunk.js"
+  },
+  {
+    "revision": "6af5007ce18af3a2008a",
+    "url": "./static/js/runtime-main.36bee524.js"
+  },
+  {
+    "revision": "5748d9283bbdb96adc3ead91282309ae",
+    "url": "./static/media/alien.5748d928.png"
+  },
+  {
+    "revision": "0027507869fa90746f52bd21780b33c1",
+    "url": "./static/media/dead.00275078.png"
+  },
+  {
+    "revision": "b1377fac621ae7ef5ea12fa3054428d5",
+    "url": "./static/media/header.b1377fac.jpg"
+  },
+  {
+    "revision": "07c1836c1a217b9f3887d6ec4e66d82e",
+    "url": "./static/media/human.07c1836c.png"
+  }
+]);
