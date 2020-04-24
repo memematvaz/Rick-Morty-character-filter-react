ae327162fe2465a74b79034c6388964018d25161
memematvaz <memematvaz@gmail.com>
2020-04-24 15:39:29 +0200

    styles-before-hover

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,10 +3,12 @@ import React from 'react';
 
 const Character = (props) => {
   return (
-    <div id={props.key}>
-      <img src={props.image} alt={props.name}/>
-      <h2>{props.name}</h2>
-      <p>{props.specie}</p>
+    <div id={props.key} className="card-character">
+      <img src={props.image} alt={props.name} className="card-img"/>
+      <div className="card-text-container">
+        <h2 className="card-title">{props.name}</h2>
+        <p className="card-specie">{props.specie}</p>
+      </div>
     </div>
   )
 }
