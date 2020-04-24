0e3e7ab0ca246f6163f0ed26663f6abe06ee05fa
memematvaz <you@example.com>
2020-04-24 10:52:52 +0200

    icons-character-details

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import alien from '../icons/alien.png';
+import human from '../icons/human.png';
+import dead from '../icons/dead.png';
+import alive from '../icons/live.png';
 
 const CharacterDetails = (props) => {
     console.log(props)
     return(
         <div>
+            <Link to="/">
+                <span className="icon fas fa-times"></span>
+            </Link>
             <img src={props.characterObject.image}
                  alt={props.characterObject.name}
             />
             <h3>{props.characterObject.name}</h3>
-            <p>{props.characterObject.species}</p>
-            <p>{props.characterObject.name} appears in {props.characterObject.episode.lenght} episodes.</p>
-            <p>Status: {props.characterObject.status}</p>
-            <Link to="/">
-                <span className="icon fas fa-times"></span>
-              </Link>
+            <p>{`${props.characterObject.name} appears in ${props.characterObject.episode.lenght} episodes.`}</p>
+            <img alt={props.characterObject.species} 
+                 src={props.characterObject.species === 'Human' ? human : alien }></img>
+            <img alt={props.characterObject.status}    
+                 src={props.characterObject.status === 'Alive' ? alive : dead }></img>
+     
+        
         </div>
     )
 }
