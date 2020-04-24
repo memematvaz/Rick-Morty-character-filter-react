95acf365ad83a987cff3f5e012668778b0042635
memematvaz <memematvaz@gmail.com>
2020-04-24 19:37:07 +0200

    finish styles

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -8,21 +8,24 @@ import alive from '../icons/live.png';
 const CharacterDetails = (props) => {
     console.log(props)
     return(
-        <div>
-            <Link to="/">
+        <div className="background-details">
+            <div className="card-details">
+             <Link to="/">
                 <span className="icon fas fa-times"></span>
-            </Link>
-            <img src={props.characterObject.image}
-                 alt={props.characterObject.name}
-            />
-            <h3>{props.characterObject.name}</h3>
-            <p>{`${props.characterObject.name} appears in ${props.characterObject.episode.length} episodes.`}</p>
-            <img alt={props.characterObject.species} 
+             </Link>
+             <img className="img-details" 
+                  src={props.characterObject.image}
+                  alt={props.characterObject.name}
+             />
+             <div className="card-text-container-details">
+             <h3>{props.characterObject.name}</h3>
+             <p>{`${props.characterObject.name} appears in ${props.characterObject.episode.length} episodes.`}</p>
+             <img alt={props.characterObject.species} 
                  src={props.characterObject.species === 'Human' ? human : alien }></img>
-            <img alt={props.characterObject.status}    
+             <img alt={props.characterObject.status}    
                  src={props.characterObject.status === 'Alive' ? alive : dead }></img>
-     
-        
+     </div>
+            </div>
         </div>
     )
 }
